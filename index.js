c1e36935dbe137ecb885a6a80174a90b581bc169
Tongxin Sun <tongxinsun16@gmail.com>
2026-07-12 15:01:43 -0700

    Finish the basic functions
    The basic functions of an etch a sketch pad have been finished. The
    default grid size is 16 * 16, but the user can customize it with the
    `reset` button.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+let numSquares = 16
 const container = document.getElementById("container")
 
-function generateDiv() {
+function generateDiv(sideLength) {
   const div = document.createElement('div')
   div.classList.add('square')
+  div.style.width = `${sideLength}px`
+  div.style.height = `${sideLength}px`
+  div.addEventListener('mouseover', () => div.classList.add('color'))
   container.appendChild(div)
 }
 
-for (let i = 0; i < 16 * 16; i++) {
-  generateDiv()
+function drawGrid() {
+  container.replaceChildren()
+  const sideLength = 960 / numSquares
+  for (let i = 0; i < numSquares * numSquares; i++) {
+    generateDiv(sideLength)
+  }
 }
+
+drawGrid()
+
+function resetGrid() {
+  do {
+    numSquares = parseInt(prompt('How many squares per side for the new grid do you want? (Max 100)'))
+  } while (numSquares > 100)
+  drawGrid()
+}
+
+const resetButton = document.getElementById('reset')
+resetButton.addEventListener('click', resetGrid)
